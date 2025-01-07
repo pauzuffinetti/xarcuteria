1372aff81e601ebd000efa003c8480f080562966
pauzuffinetti <paula.zuffinetti@gmail.com>
2025-01-07 16:12:49 -0300

    cambio jsx productos

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Products.css';
-import Producto1 from '../../assets/Producto1.jpg';
-import Producto2 from '../../assets/Producto2.jpg';
-import Producto3 from '../../assets/Producto3.jpg';
-import Producto4 from '../../assets/Producto4.jpg';
-import Producto5 from '../../assets/Producto5.jpg';
-import Producto6 from '../../assets/Producto6.jpg';
-import Producto7 from '../../assets/Producto7.jpg';
+import producto1 from '../../assets/producto1.jpg';
+import producto2 from '../../assets/producto2.jpg';
+import producto3 from '../../assets/producto3.jpg';
+import producto4 from '../../assets/producto4.jpg';
+import producto5 from '../../assets/producto5.jpg';
+import producto6 from '../../assets/producto6.jpg';
+import producto7 from '../../assets/producto7.jpg';
 
 const images = [
-  { src: Producto1, description: 'Jamón crudo' },
-  { src: Producto2, description: 'Producto' },
-  { src: Producto3, description: 'Producto' },
-  { src: Producto4, description: 'Producto' },
-  { src: Producto5, description: 'Producto' },
-  { src: Producto6, description: 'Producto' },
-  { src: Producto7, description: 'Producto' },
+  { src: producto1, description: 'Jamón crudo' },
+  { src: producto2, description: 'producto' },
+  { src: producto3, description: 'producto' },
+  { src: producto4, description: 'producto' },
+  { src: producto5, description: 'producto' },
+  { src: producto6, description: 'producto' },
+  { src: producto7, description: 'producto' },
 ];
 
 const Products = () => {
