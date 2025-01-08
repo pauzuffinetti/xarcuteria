eefb918c6f4e845793ba32da88529c6ef26a80d7
pauzuffinetti <paula.zuffinetti@gmail.com>
2025-01-07 21:24:27 -0300

    cambio tote

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/xarcuteria/',
+  base: '/xarcuteria',
   plugins: [react()],
   assetsInclude: ['**/*.JPG'], // Incluir archivos con extensión .JPG como activos
 })
